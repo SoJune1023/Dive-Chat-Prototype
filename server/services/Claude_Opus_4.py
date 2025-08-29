8d0f0f7863cc079d6678dc8ec8b42ea2b36a79b0
SoJune1023 <ssojune@naver.com>
2025-08-29 21:14:55 +0900

    remove: useless commit del

--- server/services/Claude_Opus_4.py
+++ server/services/Claude_Opus_4.py
@@ -8,10 +8,6 @@ def claude_setup_client() -> any:
     return client
 
 def claude_send_message(client: any, message_input: List[dict]): # TODO: message_inputvious: List[dict]에서 dict를 대화 schema로 변경
-    """ message_input : List[dict] 
-        {"role": "system", "content": <previous_content>},
-        {"role": "user", "content": <previous_content>}, . . .
-    """
     response = client.message.create(
         model="claude-opus-4-20250514",
         max_token=1024,
