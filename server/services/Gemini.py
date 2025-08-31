33753f18e572da98bbda872e0336eb8d45adf997
SoJune1023 <ssojune@naver.com>
2025-08-31 23:41:51 +0900

    feat: gemini service 초안 작성

---
+++ server/services/Gemini.py
@@ -0,0 +1,16 @@
+from google import genai
+from google.genai import types
+from typing import List
+
+def gemini_setup_client():
+    client = genai.Client(
+        api_key="TEMP"
+    )
+    return client
+
+def gemini_send_message(client: any, message_input: List[dict]):
+    response = client.models.generate_content(
+        model="TEMP",
+        contents="TEMP",
+        config=types.ThinkingConfig("TEMP")
+    )
