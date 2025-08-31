3b7923d41fe628c8098a5f5e75e8897b31bde8d6
SoJune1023 <ssojune@naver.com>
2025-08-31 20:15:32 +0900

    feat: Add chat-gpt service

---
+++ server/services/Chat_gpt_5_mini.py
@@ -0,0 +1,29 @@
+# <---------- Schemas ---------->
+from pydantic import BaseModel, HttpUrl
+
+class MessageItem(BaseModel):
+    said: str
+    context: str
+
+class Response(BaseModel):
+    conversation: List[MessageItem]
+    image_selected: HttpUrl
+
+# <---------- Main ---------->
+from openai import OpenAI, Client
+from typing import List
+
+def gpt_5_mini_setup_client() -> OpenAI:
+    client = OpenAI(
+        api_key="TEMP (TODO: REPLACE KEY FROM AWS)",
+        max_retries="TEMP (TODO: REPLACE VALUE FROM CONFIG)",
+        # etc. . .
+    )
+
+def gpt_5_mini_send_message(client: OpenAI, message_input: List[dict]):
+    response = client.response.create(
+        model="TEMP (TODO: REPLACE VALUE ROM CONFIG)",
+        text_format=Response,
+        input=message_input
+        # etc . . .
+    )
