dac86a988b098a5db2a455e0f09e6d4aa4b3afb1
SoJune1023 <ssojune@naver.com>
2025-09-01 12:28:04 +0900

    chore: logger 추가

--- server/main.py
+++ server/main.py
@@ -1,14 +1,16 @@
 import logging
 from flask import Flask
 
+logger = logging.getLogger(__name__)
+
 def create_app():
     try:
         app = Flask(__name__)
     except Exception as e:
-        logging.error(f"Failed to create Flask app. ErrorCode: {e}")
+        logger.error(f"Failed to create Flask app")
     
     try:
         from .routes.chat_bp import chat_bp
         app.register_blueprint(chat_bp)
     except Exception as e:
-        logging.error(f"Failed to register chat_bp. ErrorCode: {e}")
+        logger.error(f"Failed to register chat_bp")
