5f5fcb4b80d52d59ad81768d850c9180e5278cee
SoJune1023 <ssojune@naver.com>
2025-09-02 13:26:25 +0900

    refactor: services -> service

--- server/routes/chat_bp.py
+++ server/routes/chat_bp.py
@@ -1,12 +1,12 @@
 # <---------- Route ---------->
 from flask import Blueprint, jsonify, request
 
-import chat_services
+import server.routes.chat_service as chat_service
 from schemas.chat import Payload
 
 chat_bp = Blueprint('chat_bp', __name__)
 @chat_bp.route('/onSend', methods = ['POST'])
 def onSend():
     req = Payload(**request.get_json(force=True))
-    ok, code, body = chat_services.handle(req)
+    ok, code, body = chat_service.handle(req)
     return jsonify(body), code
