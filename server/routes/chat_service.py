bfdc2839e2db7a5d9b250165760621fa93a9ee43
SoJune1023 <ssojune@naver.com>
2025-09-03 13:18:42 +0900

    chore: 주석 추가

--- server/routes/chat_service.py
+++ server/routes/chat_service.py
@@ -84,6 +84,9 @@ def payload_system_flow(req: Payload) -> tuple[str, str, Optional[str], Optional
     try:
         user = req.user
         character = req.character
+        # I M P O R T A N T ! ! !
+        # 반드시 client에서 Optional이 가능 한 값은
+        # value를 Null로 둘 것!
         return (
             user.user_id, # str
             user.model, # str
