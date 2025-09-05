c317249fe16301a5bb5b8f2080ff3205b2871e9f
SoJune1023 <ssojune@naver.com>
2025-09-06 01:10:19 +0900

    feat: getenv GPT_MINI_MODEL

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ load_dotenv()  # .env 파일 로드
 
 # <--------- OpenAI ---------->
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GPT_MINI_MODEL = os.getenv("GPT_MINI_MODEL")
 
 # <--------- Google Gemini ---------->
 GEMINI_API_KEY = os.getenv("GOOGLE_API_KEY")
