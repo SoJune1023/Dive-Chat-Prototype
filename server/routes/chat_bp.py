5c9a29e13fdf78b54dd8631ba5027c42d6975b99
SoJune1023 <ssojune@naver.com>
2025-09-07 02:45:33 +0900

    fix: _ 누락 issue 해결

--- server/routes/chat_bp.py
+++ server/routes/chat_bp.py
@@ -1,12 +1,12 @@
 # <---------- Route ---------->
 from flask import Blueprint, jsonify, request
 
-from .chat_service import handle
+from .chat_service import _handle
 from schemas.chat import Payload
 
 chat_bp = Blueprint('chat_bp', __name__)
 @chat_bp.route('/onSend', methods = ['POST'])
 def onSend():
     req = Payload(**request.get_json(force=True))
-    ok, code, body = handle(req)
+    ok, code, body = _handle(req)
     return body, code
