67a50fe1866137258dd6f80098a44038835b0ede
SoJune1023 <ssojune@naver.com>
2025-09-14 13:39:57 +0900

    feat: Add SummaryPayload

--- schemas/note.py
+++ schemas/note.py
@@ -1,2 +1,15 @@
-class SummaryPayload():
-    ...
+# <---------- Schemas ----------> 
+from pydantic import BaseModel
+from typing import Optional, List
+
+class PrevConversation(BaseModel):
+    user: Optional[str]
+    system: str
+
+class SummaryPayload(BaseModel):
+    user_id: str
+    user_name: str
+
+    prevSummaryItem: List[str]
+    prevUserNote: Optional[str]
+    prevConversation: Optional[List[PrevConversation]]
