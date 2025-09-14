805afa70ccf70dc19ef2b8b0fd0faebe0c70c9b3
SoJune1023 <ssojune@naver.com>
2025-09-14 13:41:07 +0900

    fix: 잘못된 handle name 수정

--- server/routes/note_bp.py
+++ server/routes/note_bp.py
@@ -1,12 +1,12 @@
 # <---------- Route ---------->
 from flask import Blueprint, jsonify, request
 
-from .note_services import handle
+from .note_services import summary_handle
 from schemas.note import SummaryPayload
 
 note_bp = Blueprint('note_bp', __name__)
 @note_bp.route('/onSummary', methods = ['POST'])
 def onSend():
     req = SummaryPayload(**request.get_json(force=True))
-    ok, code, body = handle(req)
+    ok, code, body = summary_handle(req)
     return body, code
