a08d027cb8f8cacc20e1cc465b0fafafef604052
SoJune1023 <ssojune@naver.com>
2025-09-15 11:27:09 +0900

    feat: Add uuid.py

---
+++ server/services/uuid.py
@@ -0,0 +1,11 @@
+# <---------- def exceptions ---------->
+from ..exceptions import AppError
+
+# <---------- uuid ---------->
+from uuid_extensions import uuid7str
+
+def uuid7_builder() -> str:
+    try:
+        return uuid7str()
+    except Exception as e:
+        raise AppError("Unexpected error while build new uuid7", 500)
