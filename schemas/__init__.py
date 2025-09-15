50eb1010176e899376b57ce37a558efd9e8572d9
SoJune1023 <ssojune@naver.com>
2025-09-15 12:12:51 +0900

    feat: import schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,6 +1,6 @@
-from .ai_response import SummaryResponse
+from .ai_response import SummaryResponse, ChatResponse
 from .chat import PrevItem, User, ImgItem, Character, ChatPayload
 from .user_system import SigninPayload, RegisterPayload
 from .note import PrevConversation, SummaryPayload, UploadPayload
 
-__all__ = ['SummaryResponse', 'PrevItem', 'User', 'ImgItem', 'Character', 'ChatPayload', 'SigninPayload', 'RegisterPayload', 'PrevConversation', 'SummaryPayload', 'UploadPayload']
+__all__ = ['SummaryResponse', 'ChatResponse', 'PrevItem', 'User', 'ImgItem', 'Character', 'ChatPayload', 'SigninPayload', 'RegisterPayload', 'PrevConversation', 'SummaryPayload', 'UploadPayload']
